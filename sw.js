b3d42efaebf9c6faf9de00e9613856c46e38b436
Kim Maurice <100428390+Bertarose@users.noreply.github.com>
2026-06-10 20:27:23 -0400

    Update CACHE_VERSION to monpocha-2026-v34

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 //  Stratégie : Network First + Cache Fallback
 // ══════════════════════════════════════════════════════════
 
-const CACHE_VERSION = 'monpocha-2026-v33';
+const CACHE_VERSION = 'monpocha-2026-v34';
 
 const ASSETS = [
   '/',
