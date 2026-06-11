9b02ace010f8d14fb837bd2c67bf566c63899599
Kim Maurice <100428390+Bertarose@users.noreply.github.com>
2026-06-10 21:17:21 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 //  Stratégie : Network First + Cache Fallback
 // ══════════════════════════════════════════════════════════
 
-const CACHE_VERSION = 'monpocha-2026-v40';
+const CACHE_VERSION = 'monpocha-2026-v41';
 
 const ASSETS = [
   '/',
